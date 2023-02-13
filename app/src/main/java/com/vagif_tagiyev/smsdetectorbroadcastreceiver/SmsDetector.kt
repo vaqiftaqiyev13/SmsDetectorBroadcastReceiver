fe6d3afe79da1130f9b114e4f7dee4b9d7d89d49
Vaqif <taqiyevv937@gmail.com>
2023-02-13 19:24:13 +0400

    changed

--- app/src/main/java/com/vagif_tagiyev/smsdetectorbroadcastreceiver/SmsDetector.kt
+++ app/src/main/java/com/vagif_tagiyev/smsdetectorbroadcastreceiver/SmsDetector.kt
@@ -3,10 +3,36 @@ package com.vagif_tagiyev.smsdetectorbroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.telephony.SmsMessage
 import android.widget.Toast
 
-class SmsDetector:BroadcastReceiver() {
+class SmsDetector : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context,"You have a new message",Toast.LENGTH_SHORT).show()
+
+        val smsIntent = intent?.extras
+
+        if (smsIntent != null) {
+            val pdusObj = smsIntent.get("pdus") as Array<*>
+
+            for (i in pdusObj.indices){
+                val currentMessage:SmsMessage
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    val format = smsIntent.getString("format")
+                    currentMessage = SmsMessage.createFromPdu(pdusObj[i] as ByteArray,format)
+
+                }
+
+
+
+            }
+
+        }
+
+
+
+        Toast.makeText(context, "You have a new message", Toast.LENGTH_SHORT).show()
+
     }
 }
