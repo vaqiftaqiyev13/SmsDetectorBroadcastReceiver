d4ed7a4443a210af3b5adda0a7a17182914f7d20
Vaqif <taqiyevv937@gmail.com>
2023-02-13 16:01:31 +0400

    [MASTER] SmsDetector class was created and implemented methods from Broadcast Receiver class

---
+++ app/src/main/java/com/vagif_tagiyev/smsdetectorbroadcastreceiver/SmsDetector.kt
@@ -0,0 +1,11 @@
+package com.vagif_tagiyev.smsdetectorbroadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsDetector:BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
